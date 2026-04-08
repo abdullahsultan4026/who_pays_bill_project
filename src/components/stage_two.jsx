09ee41fb7e0d4b1a2f086e09ecc2ff5ceaac9cc4
abdullahsultan4026 <abdullahsultan4026@gmail.com>
2026-04-08 07:36:57 -0700

    Add files via upload

---
+++ src/components/stage_two.jsx
@@ -0,0 +1,24 @@
+import { useContext, useState } from "react";
+import { MyContext } from "../context";
+
+const Stage2 = () => {
+    const context = useContext(MyContext);
+    return (
+        <>
+            <div className="result_wrapper">
+                <h3>The loser is:</h3>
+                {context.result}
+            </div>
+            <div className="action_button"
+                onClick={() => context.resetGame()}>
+                Start Over
+            </div>
+            <div className="action_button  btn_2"
+                onClick={() => context.generateNewLoser()}>
+                Get new loser
+            </div>
+        </>
+    );
+}
+
+export default Stage2;
